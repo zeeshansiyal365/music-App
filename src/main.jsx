03b768aad1d41ab41f623f31117730151fc5c388
unknown <zeeshansiyal579@gmail.com>
2025-12-27 00:20:49 -0800

    final version

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app.jsx'
+import App from './appOrinal.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css'
 
